bb9239d3d51460982801002467ef48bfedd09572
Artem6evelev <artem6evelev92@gmail.com>
2023-11-06 21:50:58 +0200

    create primary navbar

---
+++ client/src/components/navbar/NavBar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Menu, Lock } from "@mui/icons-material";
+
+const NavBar = () => {
+  return (
+    <AppBar>
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <Box sx={{ mr: 1 }}>
+            <IconButton size="large" color="inherit">
+              <Menu />
+            </IconButton>
+          </Box>
+          <Typography
+            variant="h6"
+            component="h1"
+            noWrap
+            sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+          >
+            You Are Welcome
+          </Typography>
+          <Typography
+            variant="h6"
+            component="h1"
+            noWrap
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+          >
+            YRW
+          </Typography>
+          <Button color="inherit" startIcon={<Lock />}>
+            Login
+          </Button>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default NavBar;
