8172cceb605c9a4ce0a104c58380c14c70c31d8d
Artem6evelev <artem6evelev92@gmail.com>
2023-11-07 22:21:49 +0200

    create user drop menu

--- client/src/components/user/UserIcons.js
+++ client/src/components/user/UserIcons.js
@@ -1,12 +1,15 @@
 import { Mail, Notifications } from "@mui/icons-material";
 import { Avatar, Badge, Box, IconButton, Tooltip } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { useValue } from "../../context/ContextProvider";
+import UserMenu from "./UserMenu";
 
 const UserIcons = () => {
   const {
     state: { currentUser },
   } = useValue();
+
+  const [anchorUserMenu, setAnchorUserMenu] = useState(null);
 
   return (
     <Box>
@@ -21,12 +24,13 @@ const UserIcons = () => {
         </Badge>
       </IconButton>
       <Tooltip title="Open User Settings">
-        <IconButton>
+        <IconButton onClick={(e) => setAnchorUserMenu(e.currentTarget)}>
           <Avatar src={currentUser?.photoURL} alt={currentUser?.name}>
             {currentUser?.name?.charAt(0).toUpperCase()}
           </Avatar>
         </IconButton>
       </Tooltip>
+      <UserMenu {...{ anchorUserMenu, setAnchorUserMenu }} />
     </Box>
   );
 };
